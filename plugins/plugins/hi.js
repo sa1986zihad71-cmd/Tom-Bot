6bff0260de724b5aaca281b012ebf0894016852d
sa1986zihad71-cmd <sa1986zihad71@gmail.com>
2026-03-27 00:57:43 +0600

    Delete plugins/plugins directory

--- plugins/plugins/hi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'hi',
-    command: ['hi', 'hello'],
-    async execute(sock, jid, msg, text, config) {
-        await sock.sendMessage(jid, { text: `হ্যালো টম ভাই! আপনার বট একদম রেডি।` });
-    }
-};
